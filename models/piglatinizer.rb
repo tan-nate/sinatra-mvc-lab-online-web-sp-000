87bee8da2e675c947d28eaf49c1e667624774fe6
Nate Tan <nktan93@gmail.com>
2019-05-30 16:36:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,12 @@
 class PigLatinizer
   def piglatinize(text)
     text_a = text.split("")
-    pig_a = []
+
     if !text_a[0].match?(/[aeoui]/)
-      text_a.each do |c|
-        until c.match?(/[aeoui]/)
-          text_a << text_a.shift
-          binding.pry
-        end
+      until text_a[0].match?(/[aeiou]/)
+        text_a << text_a.shift
       end
-      text_a.join
+      text_a.join << "ay"
     else
       text << "way"
     end
