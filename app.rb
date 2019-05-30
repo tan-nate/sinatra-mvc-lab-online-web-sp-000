95b28e8decb51737141e832f591c62872a777402
Nate Tan <nktan93@gmail.com>
2019-05-30 17:29:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/' do
+  post '/piglatinize' do
     @piglatinizedtext = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :user_input
   end
